8ac3d9ba196957ee4859cea4acb0f8102eb21834
Keneo25 <108327914+Keneo25@users.noreply.github.com>
2023-02-23 18:48:04 +0100

    Add files via upload

---
+++ LAB 1/1_2_12/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float liczba;
+    scanf("%f",&liczba);
+    printf("%.1e", liczba); // nie wiem jak zrobiæ aby by³o 1 zamiast -001
+    return 0;
+}
