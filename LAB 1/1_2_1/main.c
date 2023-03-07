b95b16566dc485302c5c3ba36d5c616bfd2b91a2
Keneo25 <108327914+Keneo25@users.noreply.github.com>
2023-03-07 18:06:30 +0100

    Update main.c

--- LAB 1/1_2_1/main.c
+++ LAB 1/1_2_1/main.c
@@ -1,22 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//instrukcja wyjœcia:
-//przy printf
-// %d/%i - do liczb ca³kowitych %i tylko np 12 a %d moze byc 12, 012 , bezpiecznie %d uzywaæ
-// %f- uzywamy do folat
-// %lf - long float
-// %c - chatge
-// %s - string
-
-//instrukcja wejscia:
-//scanf("%d",liczba)
-
-
 int main()
 {
-    int liczba = 0;
-    printf("Podaj liczbe: ");
-    scanf(%d,liczba);
+    printf("Hello world!\n");
     return 0;
 }
